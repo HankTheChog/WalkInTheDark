9ac1137c79959d4844bd3067141786ea93a10498
Tom <tfeldmanyassur@gmail.com>
2015-08-29 04:41:56 +0300

    Added Guy's analytics and scoring scripts (not yet integrated).

---
+++ Assets/Scripts/Analytics.cs
@@ -0,0 +1,75 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Analytics : MonoBehaviour {
+	float gameStart;
+	float gameEnd;
+	int LightUse;
+
+
+	public void useLight(){
+		//call this void when using the tourch
+		LightUse++;
+	}
+
+
+	public void SetStartTime(){
+		//call this void when the game session start
+		gameStart = Time.time;
+	}
+
+
+	public void SetEndTime(){
+		//call this void when game is over
+		gameEnd = Time.time;
+		saveDate ();// saving the data collected during the game
+	}
+	void saveDate(){
+		// saving the data collected during the game
+		PlayerPrefs.SetFloat ("gameStart", gameStart);
+		PlayerPrefs.SetFloat ("gameEnd", gameEnd);
+		PlayerPrefs.SetInt ("LightUse", LightUse);
+	}
+
+
+
+
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+	////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+
+	//showing the data
+
+	
+	public void showData(){
+		//call this void to show the data from the last session
+		float startT = PlayerPrefs.GetFloat ("gameStart");
+		float endT = PlayerPrefs.GetFloat ("gameEnd");
+		int Luse = PlayerPrefs.GetInt ("LightUse");
+
+		//duration of last game as float
+		float duration = endT - startT;
+
+		//duration of last game with 2 numbers after the whole number aka 34.45
+		float readableDuration = duration * 100;
+		int tempdur = (int)readableDuration;
+		readableDuration = tempdur / 100;
+
+		//duration of last game as Int
+		int durationInt = (int)duration;
+
+
+		//pick what ever kind of way you want to show time and set it equal to timey below
+
+		float timey = readableDuration;
+
+
+
+		//string for readable data - i can add time stamp and a method to search all saved strings for deeper use
+		string lastGame = "last game session was " + timey + " seconds and the Light was used " + Luse + " times";
+		// show the data to console
+		Debug.Log (lastGame);
+	}
+
+
+}
---
+++ Assets/Scripts/ScoreCalculate.cs
@@ -0,0 +1,92 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ScoreCalculate : MonoBehaviour {
+	public float silver=110;
+	public float gold=100;
+	public float platinum=90;
+	float startTime;
+	float endTime;
+	float torcheUse;
+	public float scaleValueTime=1f;
+	public float ScaleValueTorche=5f;
+	float gameDuration;
+
+
+	public void CallOnUseTorch(){
+		//call this void when using the torch
+		torcheUse++;
+	}
+	public void CallOnStart(){
+		// call this void on game start
+		startTime = Time.time;
+	}
+	public void CallOnEnd(){
+		//call this void when game is over
+		endTime = Time.time;
+
+		gameDuration = endTime - startTime;
+
+		//calculating the score
+		CalculateScore ();
+	}
+
+	void CalculateScore(){
+		//calculating the score
+
+
+		//change the scale value to icrease/decrease the % from the total value
+		float timeValue = gameDuration *= scaleValueTime;
+		//change the scale value to icrease/decrease the % from the total value
+		float torcheValue = torcheUse *= ScaleValueTorche;
+
+		// this is the total value
+		float sum = timeValue + torcheValue;
+
+		//setting the medal based on score
+		trophyType (sum);
+	}
+	void trophyType(float sum){
+		//setting the medal based on score
+		string medal = "";
+		if (sum <= platinum) {
+			medal = "platinum";
+		}
+		if (sum <= gold&&sum>platinum) {
+			medal = "gold";
+		}
+		if (sum <= silver&&sum>gold) {
+			medal = "silver";
+		}
+		if (sum>silver) {
+			medal = "bronze";
+		}
+
+		//doing what ever needed to be done at the end
+		end (medal);
+	}
+	void end(string medal){
+		switch (medal){
+		case "platinum":
+
+			//do stuff here
+
+			break;
+		case "gold":
+			
+			//do stuff here
+
+			break;
+		case "silver":
+			
+			//do stuff here
+
+			break;
+		case "bronze":
+			
+			//do stuff here
+
+			break;
+		}
+	}
+}
